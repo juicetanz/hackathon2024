d1922b1fdea3558bebbf9b562e8f540bba2a8ae7
juicetanz <66227184+juicetanz@users.noreply.github.com>
2024-01-20 02:26:07 -0800

    p

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,16 +8,19 @@ from flask import session
 from flask import redirect
 from flask import url_for
 
+import sqlite3
+
 from PIL import Image
 
 from run import inference_image
+from . import db
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.secret_key = Flask.secret_key
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'flaskr.db'),
     )
 
     if test_config is None:
@@ -51,24 +54,51 @@ def create_app(test_config=None):
     def prediction():
         return render_template('cropprediction/prediction.html')
 
-    @app.route('/loginpage')
-    def loginpage():
-        return render_template('auth/login.html')
-    @app.route('/registerpage')
-    def registerpage():
+    @app.route('/register', methods=['GET','POST'])
+    def register():
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+            # Create variables for easy access
+            username = request.form['username']
+            password = request.form['password']
+            
+            connection = sqlite3.connect('./instance/flaskr.db')
+            print(connection)
+            connection.cursor().execute('INSERT INTO user (username,password) VALUES (?,?)', (username, password))
+            connection.close()
+            return redirect('/')
         return render_template('auth/register.html')
 
-    @app.route('/login', methods=['POST'])
+    @app.route('/login', methods=['GET','POST'])
     def login():
-        if request.method == 'POST':
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-        return '''
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+            # Create variables for easy access
+            username = request.form['username']
+            password = request.form['password']
+            print(username,password)
+            # Check if account exists using MySQL
+            connection = sqlite3.connect('./instance/flaskr.db')
+            connection.cursor().execute('SELECT * FROM user WHERE username = ? AND password = ?', (username, password))
+            # Fetch one record and return the result
+            account = connection.cursor().fetchone()
+            print(account)
+            connection.close()
+            if account:
+                # Create session data, we can access this data in other routes
+                session['username'] = request.form['username']
+                # Redirect to home page
+                return redirect('/')
+            else:
+                # Account doesnt exist or username/password incorrect
+                print("STUID NOT IN")
+                return redirect('/login')
+        return render_template('auth/login.html')
+    
+    '''
             <form method="post">
                 <p><input type=text name=username>
                 <p><input type=submit value=Login>
             </form>
-        '''
+    '''
     
     @app.route('/logout')
     def logout():
